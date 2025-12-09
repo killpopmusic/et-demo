b2c453d82c1135e05fdaa298d8b65778d74f1ab1
killpopmusic <tomek.talarczyk@gmail.com>
2025-12-09 14:20:09 +0100

    model architecture

---
+++ src/gaze_classifier.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+class GazeClassifier(nn.Module):
+    def __init__(self, input_features, num_classes=3):
+        super().__init__()
+        self.input_features = input_features
+        self.num_classes = num_classes
+
+        self.model = nn.Sequential(
+            nn.Linear(input_features,128),
+            nn.LayerNorm(128),
+            nn.SiLU(),
+            nn.Dropout(0.4),
+
+            nn.Linear(128, 128),
+            nn.LayerNorm(128),
+            nn.SiLU(),
+            nn.Dropout(0.4),
+
+            nn.Linear(128, 64),
+            nn.LayerNorm(64),
+            nn.SiLU(),
+
+            nn.Linear(64, num_classes)
+        )
+
+    def forward(self, x):
+        return self.model(x)
